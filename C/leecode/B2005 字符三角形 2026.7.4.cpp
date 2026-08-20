1ddf1eb63b0d5f666465f94bfa05f12cd379a710
xingchendahaidw <xingchendahaidw@163.com>
2026-08-21 07:18:20 +0800

    8.21

--- C/leecode/B2005 字符三角形 2026.7.4.cpp
+++ C/leecode/B2005 字符三角形 2026.7.4.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h> 
 int main(){
 	char c;
-	scanf("%c",&c);//输入，注意要取地址
+	scanf("%c",&c);
     printf("  %c  \n",c);
     printf(" %c%c%c \n",c,c,c);
     printf("%c%c%c%c%c\n",c,c,c,c,c);
